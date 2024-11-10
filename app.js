3d808340f0f617341117fd7f0e3689222c9c6305
anish1617 <basketb523@gmail.com>
2024-11-10 22:51:14 +0530

    added app.js forăse server setup

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require("express");
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+  res.type('text/plain');
+  res.send('this is thw homelage');
+})
+
+app.get('/about', function(req, res){
+  res.type('text/plain');
+  res.send('this is thw about section');
+})
+
+// custom 404 pages 
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(404);
+})
+
+
+
+// custom 500 pafe
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500- internal server error');
+
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express server started on http://localhost:' 
+    + app.get('port') + ': press ctrl + C to terminate.');
+});
